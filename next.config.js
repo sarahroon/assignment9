783fd492464af57316c57fa6cd1eff375eaca146
SARAH ROONEY <SARAHROONEY130500@GMAIL.COM>
2026-03-18 13:09:45 +0000

    message

--- next.config.js
+++ next.config.js
@@ -1,20 +1,16 @@
 // next.config.js
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
 /** @type {import('next').NextConfig} */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "pbs.twimg.com",
-        pathname: "/media/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/**",
-      },
-    ],
+  // Only add this if you need Turbopack root defined
+  turbopack: {
+    root: __dirname,
   },
 };
 
