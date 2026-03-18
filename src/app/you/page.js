a13798e669f2ab5fc71cfde52e4b8c40dc6df175
sarahroon <sarahrooney130500@gmail.com>
2026-03-18 16:40:34 +0000

    Update page.js

--- src/app/you/page.js
+++ src/app/you/page.js
@@ -1,51 +1,21 @@
-import { getUser } from "@/utils/getUser";
-import { db } from "@/utils/connect";
+// src/app/you/page.js
+import { db } from "@/utils/connect.js";
+import { getUser } from "@/utils/getUser.js";
 import Link from "next/link";
 
-export default async function UserPage() {
-  // this will always read the currently looged in user
-  // could we read from params to get other user profiles?
+export default async function YouPage() {
   const user = await getUser();
 
-  // fetch our reviews
-  const reviews = (
-    await db.query(
-      `
-            select review.*, books.title 
-            FROM review
-            JOIN books 
-            ON review.book_id = books.id
-            WHERE review.user_id = $1
-        `,
-      [user[0].id],
-    )
-  ).rows;
+  if (!user) {
+    return <p>Please log in</p>;
+  }
 
-  console.log(reviews);
   return (
     <div>
-      <p>This is the user page</p>
-      <p>{user[0].username}</p>
-      <p>bio: </p>
-      <p>{user[0].bio}</p>
-
-      <div>
-        <h2>Your reviews</h2>
-        {reviews.length === 0 ? (
-          <p>You haven`t reviewed anything yet</p>
-        ) : (
-          <ul>
-            {reviews.map((review) => (
-              <li key={review.id}>
-                <Link href={`/books/${review.book_id}`}>
-                  <strong>{review.title}</strong>
-                </Link>
-                <p>{review.content}</p>
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
+      <h1>Your Profile</h1>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
+      <Link href="/dashboard">Back to Dashboard</Link>
     </div>
   );
 }
