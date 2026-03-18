8dbdc72eb3c93744f1eea5d7b23af5a1c2254f6a
sarahroon <sarahrooney130500@gmail.com>
2026-03-18 16:40:09 +0000

    Update page.js

--- src/app/users/[id]/page.js
+++ src/app/users/[id]/page.js
@@ -1,17 +1,27 @@
-import { getUser } from "@/utils/getUser";
-import { db } from "@/utils/connect";
+// src/app/users/[id]/page.js
+import { db } from "@/utils/connect.js";
+import { getUser } from "@/utils/getUser.js";
 import Link from "next/link";
 
 export default async function UserPage({ params }) {
-  const { id } = await params;
-  // this will always read the currently looged in user
-  // could we read from params to get other user profiles?
+  const user = await getUser();
 
-  // stretch goal
+  if (!user) {
+    return <p>Please log in</p>;
+  }
+
+  const res = await db.query("SELECT * FROM user_account WHERE id = $1", [params.id]);
+  const viewedUser = res.rows[0];
+
+  if (!viewedUser) {
+    return <p>User not found</p>;
+  }
 
   return (
     <div>
-      <p>display stuff</p>
+      <h1>{viewedUser.name}</h1>
+      <p>{viewedUser.email}</p>
+      <Link href="/dashboard">Back to Dashboard</Link>
     </div>
   );
 }
