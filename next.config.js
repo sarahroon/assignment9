670e334c84f2a8c6fb972c48580583f1d4d3e6b5
sarahroon <sarahrooney130500@gmail.com>
2026-03-18 16:58:33 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  turbopack: {},
-
-  images: {
-    remotePatterns: [],
-},
-
   experimental: {
-    appDir: true,
+    turbopack: {},
   },
-},
+};
 
 export default nextConfig;
