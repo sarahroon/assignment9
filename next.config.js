41213c5e8b5d36077073af8d28755077f78f5757
sarahroon <sarahrooney130500@gmail.com>
2026-03-18 13:27:02 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,10 @@
 // next.config.js
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
-/** @type {import('next').NextConfig} */
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import path from "path";
 
 const nextConfig = {
-  reactStrictMode: true,
-  // Only add this if you need Turbopack root defined
-  turbopack: {
-    root: __dirname,
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve('./src');
+    return config;
   },
 };
 
