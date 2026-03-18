725f6aaf046f1bc9f1dc1e0b6639a4a264803753
sarahroon <sarahrooney130500@gmail.com>
2026-03-18 16:43:02 +0000

    Update connect.js

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -1,9 +1,6 @@
-// src/utils/connect.js
-import pkg from "pg";
-
-const { Pool } = pkg;
+import pg from "pg";
+const { Pool } = pg;
 
 export const db = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
