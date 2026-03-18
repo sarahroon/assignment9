dcf880bb1fcf722dd3c1175c3924066ad05a7a0f
sarahroon <sarahrooney130500@gmail.com>
2026-03-18 16:38:55 +0000

    Update page.js

--- src/app/new/page.js
+++ src/app/new/page.js
@@ -1,40 +1,19 @@
-import { db } from "@/utils/connect";
-import { getUser } from "@/utils/getUser";
+// src/app/new/page.js
+import { db } from "@/utils/connect.js";
+import { getUser } from "@/utils/getUser.js";
 import { redirect } from "next/navigation";
 
 export default async function NewBookPage() {
-  // check if user is loged in
   const user = await getUser();
 
-  async function handleAddBook(formData) {
-    "use server";
-    // pull the feilds from the form data
-    const { title, author, description, quote, released, img_url } =
-      Object.fromEntries(formData);
-
-    // inset the new book into the database
-    const result = await db.query(
-      `INSERT INTO books (title, author, description, quote, released, img_url) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`,
-      [title, author, description, quote, released, img_url],
-    );
-
-    // redirect to the new books page
-    redirect(`/books/${result.rows[0].id}`);
+  if (!user) {
+    redirect("/login");
   }
 
   return (
     <div>
       <h1>Add a New Book</h1>
-
-      <form action={handleAddBook}>
-        <input name="title" placeholder="Book title" required />
-        <input name="author" placeholder="Author" required />
-        <textarea name="description" placeholder="Description" />
-        <input name="quote" placeholder="A memorable quote" />
-        <input name="released" type="date" placeholder="Release date" />
-        <input name="img_url" placeholder="Cover image URL" />
-        <button type="submit">Add Book</button>
-      </form>
+      {/* Form goes here */}
     </div>
   );
 }
